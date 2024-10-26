fd6313af41945cd3ea007ee9a312a13cceef007d
hdcola <gh@hdcola.org>
2024-10-25 22:30:01 -0400

    init React package and setup App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,19 @@
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import './App.css';
-import CountCard from './components/CountCard';
+import { Home } from './pages/Home';
 
 function App() {
+  const queryClient = new QueryClient();
+
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <CountCard />
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Router>
+      <QueryClientProvider client={queryClient}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </QueryClientProvider>
+    </Router>
   );
 }
 
