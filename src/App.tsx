e039f2978775fd50a2dc873a5b79a94a94eb9b21
hdcola <gh@hdcola.org>
2024-10-27 22:55:20 -0400

    merge back main and app

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 import { loadStripe } from '@stripe/stripe-js';
 import './App.css';
