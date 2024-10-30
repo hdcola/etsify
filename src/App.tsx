4b06bad0e12a3c283f962ad8f55410051e8fb6dd
Tamara Plante <0631064@johnabbottcollege.net>
2024-10-30 09:22:54 -0400

    Add single item page
    Test url would look like localhost:port/items/3

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,31 @@ import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import './App.css';
 import { Home } from './pages/Home';
-import { Container } from '@mui/material';
+import { Container, useTheme, useMediaQuery } from '@mui/material';
 import Navbar from './components/Navbar';
+import SingleItem from './pages/SingleItem';
+import CssBaseline from '@mui/material/CssBaseline';
 
 function App() {
+    const theme = useTheme();
+    const lessThanXL = useMediaQuery(theme.breakpoints.down('xl'));
     const queryClient = new QueryClient();
 
     return (
-        <Container>
-            <QueryClientProvider client={queryClient}>
-                <Router>
-                    <Navbar />
-                    <Routes>
-                        <Route path="/" element={<Home />} />
-                    </Routes>
-                </Router>
-            </QueryClientProvider>
-        </Container>
+        <>
+            <CssBaseline />
+            <Container sx={{ height: '100%' }} disableGutters={lessThanXL}>
+                <QueryClientProvider client={queryClient}>
+                    <Router>
+                        <Navbar />
+                        <Routes>
+                            <Route path="/" element={<Home />} />
+                            <Route path="/items/:id" element={<SingleItem />} />
+                        </Routes>
+                    </Router>
+                </QueryClientProvider>
+            </Container>
+        </>
     );
 }
 
