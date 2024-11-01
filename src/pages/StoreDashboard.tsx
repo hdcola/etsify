00103390e0703746c7445f2044f3244e3d3b64db
iasssy <iana.setrakova@johnabbottcollege.net>
2024-11-01 10:23:45 -0400

    fixed import useState

--- src/pages/StoreDashboard.tsx
+++ src/pages/StoreDashboard.tsx
@@ -1,11 +1,11 @@
-import * as React from 'react';
+import {useState} from 'react';
 import { Stack, Box } from '@mui/material';
 import SideNav from '../components/Dashboard/SideNav';
 import ManageItems from '../components/Dashboard/ManageItems';
 import ManageOrders from '../components/Dashboard/ManageOrders';
 
 export const StoreDashboard = () => {
-    const [selectedComponent, setSelectedComponent] = React.useState('dashboard');
+    const [selectedComponent, setSelectedComponent] = useState('dashboard');
 
     const renderContent = () => {
         switch (selectedComponent) {
