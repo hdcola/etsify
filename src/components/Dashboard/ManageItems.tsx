f25b438324f390c192d804eda786188aefaeb830
iasssy <iana.setrakova@johnabbottcollege.net>
2024-11-04 00:26:10 -0500

    added price and quantity

--- src/components/Dashboard/ManageItems.tsx
+++ src/components/Dashboard/ManageItems.tsx
@@ -25,17 +25,28 @@ export default function ManageItems() {
         name: string;
         description: string;
         image_url: string;
+        quantity: number;
+        price: number;
     }
     const [items, setItems] = useState<Item[]>([]);
     const { isLoggedIn, authToken } = useLoginStore.getState();
     const apiUrl = import.meta.env.VITE_API_URL;
     const schema = yup.object().shape({
         name: yup.string().required('Store name is required'),
+        price: yup
+            .number()
+            .required('Price is required')
+            .positive('Price must be positive'),
     });
     const [formValues, setFormValues] = useState({
         name: '',
         description: '',
+        image_url: '',
+        quantity: 0,
+        price: 0,
     });
+
+    const [imageFile, setImageFile] = useState<File | null>(null);
     const [success, setSuccess] = useState<string | null>(null);
     const [error, setError] = useState<{ [key: string]: string | null }>({});
     useEffect(() => {
@@ -73,11 +84,40 @@ export default function ManageItems() {
         event.preventDefault();
         try {
             await schema.validate(formValues, { abortEarly: false });
-            await axios.post(`${apiUrl}/api/stores/items`, formValues, {
-                headers: {
-                    Authorization: `Bearer ${authToken}`,
+            const formData = new FormData();
+            formData.append('name', formValues.name);
+            formData.append('description', formValues.description);
+            formData.append('quantity', formValues.quantity.toString());
+
+            if (imageFile) {
+                formData.append('file', imageFile);
+            }
+            const uploadResponse = imageFile
+                ? await axios.post(`${apiUrl}/api/files/upload`, formData, {
+                      headers: {
+                          Authorization: `Bearer ${authToken}`,
+                          'Content-Type': 'multipart/form-data',
+                      },
+                  })
+                : null;
+
+            const imageUrl = uploadResponse ? uploadResponse.data.url : null;
+
+            await axios.post(
+                `${apiUrl}/api/stores/items`,
+                {
+                    name: formValues.name,
+                    description: formValues.description,
+                    image_url: imageUrl,
+                    price: formValues.price,
+                    quantity: formValues.quantity,
                 },
-            });
+                {
+                    headers: {
+                        Authorization: `Bearer ${authToken}`,
+                    },
+                }
+            );
 
             setSuccess('Item created successfully! Close the window');
             setError({});
@@ -97,29 +137,36 @@ export default function ManageItems() {
             setError({ general: messageError });
         }
     };
-
     const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         const { name, value } = e.target;
-        setFormValues((prev) => ({ ...prev, [name]: value }));
-        setError((prev) => ({ ...prev, [name]: null }));
+    
+        setFormValues((prev) => ({
+            ...prev,
+            [name]: (name === 'price' || name === 'quantity') && value === '' ? 0 : value,
+        }));
+    
+        setError((prev) => ({
+            ...prev,
+            [name]: null,
+        }));
     };
 
     return (
         <Stack spacing={2}>
             <Typography
-                variant="h5"
-                component="h1"
-                textAlign="center"
+                variant='h5'
+                component='h1'
+                textAlign='center'
                 sx={{ textTransform: 'uppercase' }}
             >
                 Manage Items
             </Typography>
             {isLoggedIn ? (
                 <>
-                    <Box display="inline-block">
+                    <Box display='inline-block'>
                         <Button
-                            variant="contained"
-                            size="medium"
+                            variant='contained'
+                            size='medium'
                             onClick={openCreateItemDialog}
                         >
                             Create an item <AddBoxIcon sx={{ ml: 1 }} />
@@ -130,22 +177,22 @@ export default function ManageItems() {
                             <Grid2 container spacing={2}>
                                 {items.map((item) => (
                                     <Grid2 container spacing={2}>
-                                        <Card sx={{ maxWidth: 345 }}>
+                                        <Card sx={{ maxWidth: 400 }}>
                                             <CardMedia
-                                                sx={{ height: 140 }}
+                                                sx={{ height: 250 }}
                                                 image={item.image_url}
                                                 title={item.name}
                                             />
                                             <CardContent>
                                                 <Typography
                                                     gutterBottom
-                                                    variant="h5"
-                                                    component="div"
+                                                    variant='h5'
+                                                    component='div'
                                                 >
                                                     {item.name}
                                                 </Typography>
                                                 <Typography
-                                                    variant="body2"
+                                                    variant='body2'
                                                     sx={{
                                                         color: 'text.secondary',
                                                     }}
@@ -155,8 +202,8 @@ export default function ManageItems() {
                                             </CardContent>
                                             <CardActions>
                                                 <ButtonGroup
-                                                    size="small"
-                                                    aria-label="Small button group"
+                                                    size='small'
+                                                    aria-label='Small button group'
                                                 >
                                                     <Button
                                                         onClick={handleEdit}
@@ -176,7 +223,7 @@ export default function ManageItems() {
                             </Grid2>
                         </Stack>
                     ) : (
-                        <Typography variant="body1">No items found.</Typography>
+                        <Typography variant='body1'>No items found.</Typography>
                     )}
                     <Dialog
                         open={open}
@@ -189,48 +236,101 @@ export default function ManageItems() {
                         <DialogTitle>Create an item</DialogTitle>
                         <DialogContent>
                             {error.general && (
-                                <Typography color="error">
+                                <Typography color='error'>
                                     {error.general}
                                 </Typography>
                             )}
                             {success && (
-                                <Typography color="success.main">
+                                <Typography color='success.main'>
                                     {success}
                                 </Typography>
                             )}
                             <TextField
                                 autoFocus
-                                margin="dense"
-                                label="Item Name"
-                                type="text"
-                                name="name"
+                                margin='dense'
+                                label='Item Name'
+                                type='text'
+                                name='name'
                                 value={formValues.name}
                                 onChange={handleChange}
                                 error={!!error.name}
                                 helperText={error.name || ''}
                                 fullWidth
-                                variant="outlined"
+                                variant='outlined'
                             />
                             <TextField
-                                margin="dense"
-                                label="Item Description"
-                                type="text"
-                                name="description"
+                                margin='dense'
+                                label='Item Description'
+                                type='text'
+                                name='description'
                                 value={formValues.description}
                                 onChange={handleChange}
                                 error={!!error.description}
                                 helperText={error.description || ''}
                                 fullWidth
-                                variant="outlined"
+                                variant='outlined'
                                 multiline
                                 rows={5}
                             />
+                            <Typography
+                                variant='body2'
+                                color='text.secondary'
+                                sx={{ mt: 2 }}
+                            >
+                                Item Image (JPG, GIF, PNG, max 10 MB)
+                            </Typography>
+
+                            <Box display='block' mt={1}>
+                                {imageFile && (
+                                    <Typography>{imageFile.name}</Typography>
+                                )}
+                                <Button variant='contained' component='label'>
+                                    Upload image
+                                    <input
+                                        type='file'
+                                        hidden
+                                        accept='.jpg,.gif,.png'
+                                        onChange={(e) => {
+                                            if (e.target.files) {
+                                                setImageFile(e.target.files[0]);
+                                            }
+                                        }}
+                                    />
+                                </Button>
+                            </Box>
+
+                            <Box display='block' mt={1}>
+                                <TextField
+                                    margin='dense'
+                                    label='Quantity in stock'
+                                    type='number'
+                                    name='quantity'
+                                    value={formValues.quantity}
+                                    onChange={handleChange}
+                                    error={!!error.quantity}
+                                    helperText={error.quantity || ''}
+                                    variant='outlined'
+                                />
+                            </Box>
+                            <Box display='block' mt={1}>
+                                <TextField
+                                    margin='dense'
+                                    label='Price'
+                                    type='number'
+                                    name='price'
+                                    value={formValues.price ?? 0}
+                                    onChange={handleChange}
+                                    error={!!error.price}
+                                    helperText={error.price || ''}
+                                    variant='outlined'
+                                />
+                            </Box>
                         </DialogContent>
                         <DialogActions>
                             <Button onClick={closeCreateItemDialog}>
                                 Close
                             </Button>
-                            <Button variant="contained" type="submit">
+                            <Button variant='contained' type='submit'>
                                 Save
                             </Button>
                         </DialogActions>
